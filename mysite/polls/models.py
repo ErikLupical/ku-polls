94584f0ae02bac3fb1d3d8f175ead5839921d6f9
Oak Soe Htet <oaksoe.h@ku.th>
2024-09-16 01:31:18 +0700

    added docstrings

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,21 +5,53 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
+    """
+    Represents a question in the poll with a text and publication dates.
+    
+    Attributes:
+        question_text (str): The text of the question.
+        pub_date (datetime): The date when the question was published.
+        end_date (datetime): The date when the question ends (optional).
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', default=timezone.now)
     end_date = models.DateTimeField('end date', blank=True, null=True)
     
     def __str__(self):
+        """
+        Returns a string representation of the question.
+        
+        Returns:
+            str: The text of the question.
+        """
         return self.question_text
     
     def is_published(self):
+        """
+        Determines if the question is published.
+        
+        Returns:
+            bool: True if the question is published; otherwise, False.
+        """
         return self.pub_date <= timezone.now()
     
     def was_published_recently(self):
+        """
+        Determines if the question was published within the last day.
+        
+        Returns:
+            bool: True if the question was published within the last day; otherwise, False.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
     def can_vote(self):
+        """
+        Determines if voting is allowed for the question based on its publication and end dates.
+        
+        Returns:
+            bool: True if voting is allowed; otherwise, False.
+        """
         now = timezone.now()
         if self.end_date is None:
             return self.pub_date <= now
@@ -31,13 +63,33 @@ class Question(models.Model):
         description='Published recently?',
     )
     def was_published_recently(self):
+        """
+        Display whether the question was published recently for the admin interface.
+        
+        Returns:
+            bool: True if the question was published within the last day; otherwise, False.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
+    """
+    Represents a choice for a question in the poll.
+    
+    Attributes:
+        question (Question): The question that this choice belongs to.
+        choice_text (str): The text of the choice.
+        votes (int): The number of votes for this choice.
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     
     def __str__(self):
+        """
+        Returns a string representation of the choice.
+        
+        Returns:
+            str: The text of the choice.
+        """
         return self.choice_text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,6 +9,13 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
+    """
+    View to display the list of the latest questions.
+    
+    Attributes:
+        template_name (str): The template to use for rendering this view.
+        context_object_name (str): The name of the context variable to use in the template.
+    """
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
@@ -16,6 +23,10 @@ class IndexView(generic.ListView):
         """
         Return the last five published questions (not including those set to be
         published in the future).
+
+        Returns:
+            QuerySet: A QuerySet of the latest five published questions.
+
         """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
@@ -23,18 +34,35 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
+    """
+    View to display details of a specific question.
+    
+    Attributes:
+        model (Model): The model to use for this view.
+        template_name (str): The template to use for rendering this view.
+    """
     model = Question
     template_name = 'polls/detail.html'
     
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
+        Returns:
+            QuerySet: A QuerySet of questions that are published.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
     
     def get(self, request, *args, **kwargs):
         """
         Handle GET requests. Redirect to index page with an error message if voting is not allowed.
+        
+        Args:
+            request (HttpRequest): The request object.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+        
+        Returns:
+            HttpResponse: A redirect response if voting is not allowed, or the standard GET response otherwise.
         """
         question = self.get_object()
         if not question.can_vote():
@@ -46,11 +74,28 @@ class DetailView(generic.DetailView):
 
 
 class ResultsView(generic.DetailView):
+    """
+    View to display the results of a specific question.
+    
+    Attributes:
+        model (Model): The model to use for this view.
+        template_name (str): The template to use for rendering this view.
+    """
     model = Question
     template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
+    """
+    Handle the voting process for a specific question.
+    
+    Args:
+        request (HttpRequest): The request object.
+        question_id (int): The ID of the question to vote on.
+        
+    Returns:
+        HttpResponseRedirect: Redirects to the results page of the question after processing the vote.
+    """
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
